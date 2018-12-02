07a1b475e9e7ba81423e2215a7de2953bf4699c8
brodaua <sky1nax@gmail.com>
2018-12-02 19:20:48 +0000

    add experiments + writing network output to file

--- load_and_test.py
+++ load_and_test.py
@@ -12,7 +12,7 @@ WIDTH = 227
 HEIGHT = 227
 
 logfile = "test.log"
-test_txt = "test_set.txt"
+test_txt = 'Girl2.txt'#"test_set.txt"
 def load_train_test_set(train_file):
     '''
     return train_set or test_set
@@ -48,11 +48,9 @@ def data_reader(input_queue):
     target_img = tf.read_file(input_queue[1])
 
     search_tensor = tf.to_float(tf.image.decode_jpeg(search_img, channels = 3))
-    search_tensor = tf.image.resize_images(search_tensor,[HEIGHT,WIDTH],
-                            method=tf.image.ResizeMethod.BILINEAR)
+    search_tensor = tf.image.resize_images(search_tensor,[HEIGHT,WIDTH], method=tf.image.ResizeMethod.BILINEAR)
     target_tensor = tf.to_float(tf.image.decode_jpeg(target_img, channels = 3))
-    target_tensor = tf.image.resize_images(target_tensor,[HEIGHT,WIDTH],
-                            method=tf.image.ResizeMethod.BILINEAR)
+    target_tensor = tf.image.resize_images(target_tensor,[HEIGHT,WIDTH], method=tf.image.ResizeMethod.BILINEAR)
     box_tensor = input_queue[2]
     return [search_tensor, target_tensor, box_tensor]
 
@@ -72,8 +70,7 @@ def next_batch(input_queue):
 if __name__ == "__main__":
     if (os.path.isfile(logfile)):
         os.remove(logfile)
-    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
-        level=logging.DEBUG,filename=logfile)
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG,filename=logfile)
 
     [train_target, train_search, train_box] = load_train_test_set(test_txt)
     target_tensors = tf.convert_to_tensor(train_target, dtype=tf.string)
@@ -103,15 +100,24 @@ if __name__ == "__main__":
         saver = tf.train.Saver()
         saver.restore(sess, ckpt.model_checkpoint_path)
     try:
-        for i in range(0, int(len(train_box)/BATCH_SIZE)):
-            cur_batch = sess.run(batch_queue)
-            start_time = time.time()
-            [batch_loss, fc4] = sess.run([tracknet.loss, tracknet.fc4],feed_dict={tracknet.image:cur_batch[0],
-                    tracknet.target:cur_batch[1], tracknet.bbox:cur_batch[2]})
-            logging.info('batch box: %s' %(fc4))
-            logging.info('gt batch box: %s' %(cur_batch[2]))
-            logging.info('batch loss = %f'%(batch_loss))
-            logging.debug('test: time elapsed: %.3fs.'%(time.time()-start_time))
+        with open('./test_bbox.txt','w') as ftest:
+            for i in range(0, int(len(train_box)/BATCH_SIZE)):
+                cur_batch = sess.run(batch_queue)
+                start_time = time.time()
+                [batch_loss, fc4] = sess.run([tracknet.loss, tracknet.fc4],
+                    feed_dict={
+                        tracknet.image:cur_batch[0],
+                        tracknet.target:cur_batch[1], 
+                        tracknet.bbox:cur_batch[2]})
+                logging.info('batch box: %s' %(fc4))
+                logging.info('gt batch box: %s' %(cur_batch[2]))
+                logging.info('batch loss = %f'%(batch_loss))
+                logging.debug('test: time elapsed: %.3fs.'%(time.time()-start_time))
+
+                for line in fc4:
+                    ftest.write(',,')
+                    ftest.write(','.join(map(str,line/10)))
+                    ftest.write('\n')
     except KeyboardInterrupt:
         print("get keyboard interrupt")
 
